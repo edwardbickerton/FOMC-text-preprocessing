4e0ba72d59a40dbeb59062b3287605ea8617a4bd
Edward <83250017+rw19842@users.noreply.github.com>
2023-08-14 02:00:16 +0100

    update process_dataset

--- global_vars.py
+++ global_vars.py
@@ -3,6 +3,7 @@ from hidden_vars import DATA_DIR
 
 ALL_DOCS_FILE = "fomc_documents.csv"
 ALL_DOCS_FILE_PATH = DATA_DIR + ALL_DOCS_FILE
+PREPROCESSED_DATA_DIR = "preprocessed_data/"
 MAX_N_GRAM_LENGTH = 4
 N_GRAM_FILE_PATH = "n_grams.csv"
 NUM_DOCS = len(pd.read_csv(ALL_DOCS_FILE_PATH))
--- process_dataset.py
+++ process_dataset.py
@@ -1,64 +1,19 @@
 import pandas as pd
-from nltk.tokenize import sent_tokenize, word_tokenize
-from preprocessing_rules import (
-    remove_urls,
-    capitalisation_normalisation,
-    remove_accents,
-    expand_contractions,
-    remove_punctuation,
-    remove_numbers,
-    remove_empty,
-)
-from hidden_vars import DATA_DIR
+from tqdm import tqdm
 
-ALL_DOCS_FILE = "fomc_documents.csv"
-ALL_DOCS_FILE_PATH = DATA_DIR + ALL_DOCS_FILE
-
-
-def yield_document(path):
-    try:
-        for chunk in pd.read_csv(path, usecols=["text"], chunksize=2**10):
-            documents = chunk["text"].to_list()
-            for document in documents:
-                yield document
-
-    except FileNotFoundError:
-        print(f"The path provided for your dataset does not exist: {path}")
-        exit()
-    except ValueError:
-        print("The CSV file has no 'text' column.")
-        exit()
-
-
-def yield_sentence(path):
-    for document in yield_document(path):
-        for sentence in sent_tokenize(document):
-            yield sentence
-
-
-def process_sentence(sentence):
-    sentence = remove_urls(sentence)
-
-    sentence = expand_contractions(sentence)
-
-    sentence = capitalisation_normalisation(sentence)
-
-    sentence = remove_accents(sentence)
-
-    word_list = word_tokenize(sentence)
-
-    word_list = remove_punctuation(word_list)
-
-    word_list = remove_numbers(word_list)
-
-    word_list = remove_empty(word_list)
-
-    # MAYBE: n_grams, remove stop words, lemmatization
-
-    return word_list
-
+from configurations import baseline_config, lightweight_config, heavyweight_config
+from global_vars import ALL_DOCS_FILE_PATH, PREPROCESSED_DATA_DIR, NUM_DOCS
 
 if __name__ == "__main__":
-    with open("clean_sentences.txt", "a") as file:
-        for sentence in yield_sentence(ALL_DOCS_FILE_PATH):
-            file.write(" ".join(process_sentence(sentence)) + "\n")
+    all_fomc_documents = pd.read_csv(ALL_DOCS_FILE_PATH)
+    for config in [baseline_config, lightweight_config, heavyweight_config]:
+        tqdm.pandas(
+            total=NUM_DOCS,
+            desc=f"Applying {config.name} preprocessing",
+            unit="documents",
+        )
+        df = all_fomc_documents.copy()
+        df["text"] = df["text"].progress_apply(
+            lambda document: list(config.preprocess_document_string(document))
+        )
+        df.to_csv(f"{PREPROCESSED_DATA_DIR}{config.name}.csv", index=False)
